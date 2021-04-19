2b936516302b8f01c2e8aba5b68e36adb8ed8d26
Dunieski Otano <dunieskior@gmail.com>
2021-04-19 14:23:26 -0400

    Functions

---
+++ Functions/functions-improved.py
@@ -0,0 +1,58 @@
+import os, math
+from os import system 
+from colorama import Fore, Back, Style 
+from math import pow 
+
+system('clear')
+
+isNotZero = False 
+
+# --- Functions ----
+#        2     3
+def calculate(num1, num2, marker):
+    if(marker == 'add'):
+        operation = num1 + num2 
+    elif(marker == 'substract'):
+        operation = num1 - num2 
+    elif(marker == 'multiply'):
+        operation = num1 * num2 
+    elif(marker == 'divide'):
+        if (num2 == 0):
+            try:
+                operation = num1 / num2 
+            except ZeroDivisionError: print(f'Not possible division by {n2}')
+        else:
+            operation = num1 / num2 
+    elif(marker == 'modulus'):
+        operation = num1 % num2 
+    elif(marker == 'power1'):
+        operation = num1 ** num2 
+    else:
+        operation = pow(num1, num2)
+ 
+    return operation
+
+
+# ----------- User input -----
+n1 = int(input('Enter first number >> '))
+n2 = int(input('Enter second number >> '))
+# -------- end of User input ----
+
+# ---- Calling functions -----
+addition = calculate(n1, n2, 'add') 
+subtraction = calculate(n1, n2, 'subtract')
+multiplication = calculate(n1, n2, 'multiply')
+division = calculate(n1, n2, 'divide')
+modulus = calculate(n1, n2, 'modulus')
+exponent1 = calculate(n1, n2, 'power1')
+exponent2 = calculate(n1, n2, 'power2')
+# ---- end of calling functions -----
+
+# -- Print values ----
+print(f'The sum of {n1} and {n2} is {addition}')
+print(f'The subtraction of {n1} and {n2} is {subtraction}')
+print(f'The multiplication of {n1} and {n2} is {multiplication}')
+print(f'The division of {n1} and {n2} is {division}')
+print(f'The modulus from division of {n1} and {n2} is {modulus}')
+print(f'{n1} elevated to the power of {n2} is {exponent1} from function powerOf1')
+print(f'{n1} elevated to the power of {n2} is {exponent2} from function powerOf2')
--- Functions/functions.py
+++ Functions/functions.py
@@ -1,6 +1,7 @@
-import os 
+import os, math
 from os import system 
 from colorama import Fore, Back, Style 
+from math import pow 
 
 system('clear')
 
@@ -30,11 +31,29 @@ def divide(num1, num2):
     else:
         divideCalculate = num1 / num2
         return divideCalculate
+
+def printModulus(num1, num2):
+    modulusCalculate = num1 % num2
+    return modulusCalculate
+
+def powerOf1(num1, num2):
+    exponent = num1 ** num2
+    return exponent
+
+def powerOf2(num1, num2):
+    exponent = pow(n1, n2)
+    return exponent
+
+def printName(fname, lname, age):
+    print(f'{fname} {lname}!! Cool name! I like it!!! and you are {age} years old')
 # -------------------
 
 # ----------- User input -----
 n1 = int(input('Enter first number >> '))
 n2 = int(input('Enter second number >> '))
+fname = input("What is your first name? >> ")
+lname = input("What is your last name? >> ")
+age = int(input("What is your age? >> "))
 # -------- end of User input ----
 
 # ---- Calling functions -----
@@ -44,6 +63,12 @@ addition = add(n1, n2) # n1 and n2 are arguments
 subtraction = subtract(n1, n2)
 multiplication = multiply(n1, n2)
 division = divide(n1, n2)
+modulus = printModulus(n1, n2)
+exponent1 = powerOf1(n1, n2)
+exponent2 = powerOf2(n1, n2)
+
+
+
 
 # ---- end of calling functions -----
 
@@ -51,5 +76,13 @@ print(f'The sum of {n1} and {n2} is {addition}')
 print(f'The subtraction of {n1} and {n2} is {subtraction}')
 print(f'The multiplication of {n1} and {n2} is {multiplication}')
 print(f'The division of {n1} and {n2} is {division}')
+print(f'The modulus from division of {n1} and {n2} is {modulus}')
+print(f'{n1} elevated to the power of {n2} is {exponent1} from function powerOf1')
+print(f'{n1} elevated to the power of {n2} is {exponent2} from function powerOf2')
+
+print()
+print("---------------------------------")
+printName(fname, lname, age)
+print("---------------------------------")
 
 
